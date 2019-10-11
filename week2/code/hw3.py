da9ac92edb315936d1959975143fa2921b722a5d
CoodingPenguin <unodostre@naver.com>
2019-10-11 23:47:23 +0900

    2주차 과제 완료

---
+++ week2/code/hw3.py
@@ -0,0 +1,15 @@
+# Level 1 : 순서대로 별 찍기
+for i in range(1, 11):
+    print('*'*i)
+
+print()
+
+# Level 2 : 2단위로 별 찍기
+for i in range(1, 10, 2):
+    print('*'*i)
+
+print()
+
+# Level 3 : 거꾸로 별 찍기
+for i in range(10, 0, -1):
+    print('*'*i)
