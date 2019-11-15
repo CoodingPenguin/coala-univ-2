251c55893d8565be1eddce9f0a4a95155321c731
CoodingPenguin <unodostre@naver.com>
2019-11-15 19:25:42 +0900

    Make Numpy Practice 2

--- week6/challenge1.py
+++ week6/challenge1.py
@@ -6,13 +6,15 @@ driver = webdriver.Chrome('./chromedriver')
 # 드라이버를 켜서 이 페이지로 접속
 driver.get('https://papago.naver.com/?sk=auto&tk=ko')
 
+time.sleep(3)
+
 # 단어 입력하기
 input_box = driver.find_element_by_css_selector('textarea#txtSource')
 input_box.send_keys('seize the day.')
 
-# 번역 버튼 누르기
-translate_button = driver.find_element_by_css_selector('button#btnTranslate')
-translate_button.click()
+# # 번역 버튼 누르기
+# translate_button = driver.find_element_by_css_selector('button#btnTranslate')
+# translate_button.click()
 
 # 1초 delay
 time.sleep(1)
